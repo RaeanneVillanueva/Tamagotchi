ad9542bca75395b03a582915bf8761f81d59d378
RaeanneVillanueva <raeanne1999@gmail.com>
2019-11-22 20:07:22 +0800

    designed the app

--- app/src/main/java/com/example/tamagotchi/MainActivity.java
+++ app/src/main/java/com/example/tamagotchi/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         hungerMeter = findViewById(R.id.hungerMeter);
 
